0d49baaa1cf15c7637a0624121afdff260f50e87
virtualvasu <virtualvasu624@gmail.com>
2026-02-23 20:45:58 +0530

    complete restructure

--- government-frontend/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-interface Window {
-  ethereum?: {
-    isMetaMask?: boolean;
-    request: (args: { method: string; params?: any[] }) => Promise<any>;
-    on?: (event: string, handler: (...args: any[]) => void) => void;
-    removeListener?: (event: string, handler: (...args: any[]) => void) => void;
-    selectedAddress?: string;
-  };
-}
